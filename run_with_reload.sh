b3c51d60b3add23a1ff50b31bb0346b54a2deee4
TimurMeshchenko <matsuokisho@gmail.com>
2024-04-24 00:01:46 +0300

    rewritten for nginx

--- run_with_reload.sh
+++ run_with_reload.sh
@@ -4,11 +4,11 @@
 restart_gunicorn() {
     echo "Restarting Gunicorn..."
     pkill -f uvicorn
-    uvicorn store.asgi:application &
+    uvicorn store.asgi:application --port 8001 &
 }
 
 # Start Gunicorn
-uvicorn store.asgi:application &
+uvicorn store.asgi:application --port 8001 &
 
 # Watch for changes in the project directory and restart Gunicorn when necessary
 while inotifywait -r -e modify,move,create,delete /home/deb/Python/pet_projects/store_with_payment; do 
